34322490ff72f62dbebb82ed123f2849b313e637
CasparB <casparbroekhuizen@gmail.com>
2023-10-31 15:40:32 -0700

    clock divider updated test

--- clock_divider_test.v
+++ clock_divider_test.v
@@ -3,9 +3,18 @@ module clock_divder_test;
 	reg clk;
 	reg rst;
 
+  	reg clk_1Hz;
+  	reg clk_2Hz;
+  	reg clk_500Hz;	
+  	reg clk_5Hz;
+  
 	clock_divider uut (
 		.clk(clk),
-		.rst(rst)
+      	.rst(rst),
+      	.clk_1Hz(clk_1Hz),
+      	.clk_2Hz(clk_2Hz),
+      	.clk_500Hz(clk_500Hz),
+      	.clk_5Hz(clk_5Hz)
 	);
 
 	initial begin
