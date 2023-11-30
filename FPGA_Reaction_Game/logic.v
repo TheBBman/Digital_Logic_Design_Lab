88983e2a186745a68212965d8283149523fcb62b
CasparB <casparbroekhuizen@gmail.com>
2023-11-30 15:19:38 -0800

    rst 2

--- FPGA_Reaction_Game/logic.v
+++ FPGA_Reaction_Game/logic.v
@@ -27,52 +27,56 @@ parameter hard_ticks =  100000;
 reg [19:0] tick_count;
 
 always @(posedge clk_20Hz or posedge rst) begin
-    // Select
-    if (btnS && ~btnS_lock) begin
-        btnS_lock <= 1;
-        if (select == 0)
-            select <= 1; // Random number mode
-        else if (select == 1) begin
-            select <= 2; // Counting mode
-            number <= 0;
-        end
-        else if (select == 2)
-            select <= 3; // Score calculation mode
-    end
-    if (~btnS && btnS_lock)
-        btnS_lock <= 0;
-    
-    // Up
-    if (btnU && ~btnU_lock) begin
-        btnU_lock <= 1;
-        if (select == 0) begin
-            if (mode < 2)
-                mode <= mode + 1;
-        end
-    end
-    if (~btnU && btnU_lock)
+    if (rst) begin
         btnU_lock <= 0;
-
-    // Down
-    if (btnD && ~btnD_lock) begin
-        btnD_lock <= 1;
-        if (select == 0) begin
-            if (mode > 0)
-                mode <= mode - 1;
-        end
-    end
-    if (~btnD && btnD_lock)
+        btnS_lock <= 0;
         btnD_lock <= 0;
+        select <= 0;
+        mode <= 1;
+
+    end else begin
+        // Select
+        if (btnS && ~btnS_lock) begin
+            btnS_lock <= 1;
+            if (select == 0)
+                select <= 1; // Random number mode
+            else if (select == 1) begin
+                select <= 2; // Counting mode
+                number <= 0;
+            end
+            else if (select == 2)
+                select <= 3; // Score calculation mode
+        end
+        if (~btnS && btnS_lock)
+            btnS_lock <= 0;
+        
+        // Up
+        if (btnU && ~btnU_lock) begin
+            btnU_lock <= 1;
+            if (select == 0) begin
+                if (mode < 2)
+                    mode <= mode + 1;
+            end
+        end
+        if (~btnU && btnU_lock)
+            btnU_lock <= 0;
+
+        // Down
+        if (btnD && ~btnD_lock) begin
+            btnD_lock <= 1;
+            if (select == 0) begin
+                if (mode > 0)
+                    mode <= mode - 1;
+            end
+        end
+        if (~btnD && btnD_lock)
+            btnD_lock <= 0;
+    end
 end
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        select <= 0;
-        mode <= 1;
         led <= 0;
-        btnU_lock <= 0;
-        btnS_lock <= 0;
-        btnD_lock <= 0;
         tick_count <= 0;
         current_rand <= 0;
         number <= 0;
