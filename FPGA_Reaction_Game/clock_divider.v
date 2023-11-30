5b3fdfd4d1cc33b9e01592e49a83963a8410573b
CasparB <casparbroekhuizen@gmail.com>
2023-11-30 15:07:12 -0800

    refactor

--- FPGA_Reaction_Game/clock_divider.v
+++ FPGA_Reaction_Game/clock_divider.v
@@ -4,14 +4,14 @@ module clock_divider(
     input clk,            // Master clock input, 100MHz
     input rst,            // Reset signal
     output reg clk_2Hz,   // 2Hz clock output
-    output reg clk_5Hz,    // 5Hz flashing clock
-    output reg clk_10Hz,
+    output reg clk_5Hz,   
+    output reg clk_20Hz,
     output reg clk_500Hz  // 500Hz clock output
 );
 
 reg [26:0] counter_2Hz;
 reg [26:0] counter_5Hz;
-reg [26:0] counter_10Hz;
+reg [26:0] counter_20Hz;
 reg [16:0] counter_500Hz;
 
 always @(posedge clk or posedge rst) begin
@@ -22,7 +22,7 @@ always @(posedge clk or posedge rst) begin
         counter_500Hz <= 0;
         clk_2Hz <= 0;
         clk_5Hz <= 0;
-        clk_10Hz <= 0;
+        clk_20Hz <= 0;
         clk_500Hz <= 0;
 
     end else begin
@@ -42,11 +42,12 @@ always @(posedge clk or posedge rst) begin
             counter_5Hz <= counter_5Hz + 1;
         end
         
-        if (counter_10Hz == 4999999) begin
-            counter_10Hz <= 0;
-            clk_10Hz <= ~clk_10Hz;
+        // Generate 20Hz clock
+        if (counter_20Hz == 2499999) begin
+            counter_20Hz <= 0;
+            clk_20Hz <= ~clk_20Hz;
         end else begin
-            counter_10Hz <= counter_10Hz + 1;
+            counter_20Hz <= counter_20Hz + 1;
         end
 
         // Generate 500Hz clock
--- FPGA_Reaction_Game/logic.v
+++ FPGA_Reaction_Game/logic.v
@@ -5,6 +5,7 @@ module logic(
     input btnD,
     input btnS,
     input [13:0] rand,
+    input clk_20Hz,
     output reg [1:0] select,
     output reg [1:0] mode,
     output reg [13:0] number,
@@ -14,6 +15,7 @@ module logic(
 reg btnU_lock;
 reg btnS_lock;
 reg btnD_lock;
+
 reg [13:0] current_rand;
 reg [12:0] score;
 reg [4:0] num_leds_off;
@@ -23,6 +25,45 @@ parameter reg_ticks =   200000;
 parameter hard_ticks =  100000;
 
 reg [19:0] tick_count;
+
+always @(posedge clk_20Hz) begin
+    // Select
+    if (btnS && ~btnS_lock) begin
+        btnS_lock <= 1;
+        if (select == 0)
+            select <= 1; // Random number mode
+        else if (select == 1) begin
+            select <= 2; // Counting mode
+            number <= 0;
+        end
+        else if (select == 2)
+            select <= 3; // Score calculation mode
+    end
+    if (~btnS && btnS_lock)
+        btnS_lock <= 0;
+    
+    // Up
+    if (btnU && ~btnU_lock) begin
+        btnU_lock <= 1;
+        if (select == 0) begin
+            if (mode < 2)
+                mode <= mode + 1;
+        end
+    end
+    if (~btnU && btnU_lock)
+        btnU_lock <= 0;
+
+    // Down
+    if (btnD && ~btnD_lock) begin
+        btnD_lock <= 1;
+        if (select == 0) begin
+            if (mode > 0)
+                mode <= mode - 1;
+        end
+    end
+    if (~btnD && btnD_lock)
+        btnD_lock <= 0;
+end
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
@@ -36,31 +77,13 @@ always @(posedge clk or posedge rst) begin
         current_rand <= 0;
         number <= 0;
     end else begin
-        if (select == 0) begin
-            if (btnS) begin
-                select <= 3;
-                btnS_lock <= 1;
-            end
-            if (~btnU_lock && btnU) begin
-                if (mode < 2) begin
-                    mode <= mode + 1;
-                end
-                btnU_lock <= 1;
-            end
-            if (btnU_lock && ~btnU) begin
-                btnU_lock <= 0;
-            end
-            if (~btnD_lock && btnD) begin
-                if (mode > 0) begin
-                    mode <= mode - 1;
-                end
-                btnD_lock <= 1;
-            end
-            if (btnD_lock && ~btnD) begin
-                btnD_lock <= 0;
+        if (select == 1) begin
+            if (current_rand == 0) begin
+                current_rand = rand;
+                number = current_rand;
             end
         end
-        if (select == 1) begin
+        if (select == 2) begin
             tick_count <= tick_count + 1;
             if (mode == 0 && (tick_count == easy_ticks - 1)) begin
                 number <= number + 1;
@@ -74,14 +97,8 @@ always @(posedge clk or posedge rst) begin
                 number <= number + 1;
                 tick_count <= 0;
             end
-            if (btnS_lock && ~btnS) begin
-                btnS_lock <= 0;
-            end
-            if (btnS && ~btnS_lock) begin
-                select <= 2;
-            end
         end
-        if (select == 2) begin
+        if (select == 3) begin
             if (number < current_rand)
                 score <= current_rand - number;
             else 
@@ -90,20 +107,6 @@ always @(posedge clk or posedge rst) begin
             if (score < 490) begin
                 num_leds_off = score/30;
                 led = ~((1'b1 << num_leds_off) - 1'b1);
-            end
-        end
-        if (select == 3) begin
-            if (current_rand == 0) begin
-                current_rand = rand;
-                number = current_rand;
-            end
-            if (btnS_lock && ~btnS) begin
-                btnS_lock <= 0;
-            end
-            if (btnS && ~btnS_lock) begin
-                select <= 1;
-                number <= 0;
-                btnS_lock <= 1;
             end
         end
     end
